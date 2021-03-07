6e6d8479b8a6d7d870e019ed438f05767d4f1889
only-music <79091681+only-music@users.noreply.github.com>
2021-03-08 04:19:10 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,8 +1,18 @@
 # import logging
 from pyrogram import Client, idle
 
-app = Client("tgvc")
-# logging.basicConfig(level=logging.INFO)
+api_id = 3164029
+api_hash = "d9441a47ed5187616613fb6d61ee0208"
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc.player",
+        "ping"
+    ]
+)
+
+app = Client("tgvc", api_id, api_hash, plugins=plugins)
 app.start()
 print('>>> USERBOT STARTED')
 idle()
